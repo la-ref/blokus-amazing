ca55ca62bd3f0750b86b7a19a6ed362152450ab7
MisterGranti67 <mathsgovlog@gmail;com>
2022-11-16 17:39:48 +0100

    Bouton

---
+++ Accueil/test.py
@@ -0,0 +1,97 @@
+import tkinter as tk
+
+
+class RoundedButton(tk.Canvas):
+
+    def __init__(self, master=None, text:str="", hauteur=20, largueur=20, fontsize=20, radius=25, btnforeground="#000000", btnbackground="#ffffff", clicked=None, *args, **kwargs):
+        super(RoundedButton, self).__init__(master, *args, **kwargs)
+        self.config(bg=self.master["bg"])
+        self.btnbackground = btnbackground
+        self.clicked = clicked
+        self.largueur = largueur
+        self.radius = radius
+        self.hauteur = hauteur   
+        
+        self.rect = self.round_rectangle(0, 0, 0, 0, tags="button", radius=radius, fill=btnbackground)
+        self.text = self.create_text(0, 0, text=text, tags="button", fill=btnforeground, font=("Times", 30), justify="center")
+
+        self.tag_bind("button", "<ButtonPress>", self.border)
+        self.tag_bind("button", "<ButtonRelease>", self.border)
+        self.bind("<Configure>", self.resize)
+        
+        text_rect = self.bbox(self.text)
+        self["width"] = self.largueur
+        
+        self["height"] = self.hauteur
+          
+    def round_rectangle(self, x1, y1, x2, y2, radius=25, update=False, **kwargs): # if update is False a new rounded rectangle's id will be returned else updates existing rounded rect.
+        # source: https://stackoverflow.com/a/44100075/15993687
+        points = [x1+radius, y1,
+                x1+radius, y1,
+                x2-radius, y1,
+                x2-radius, y1,
+                x2, y1,
+                x2, y1+radius,
+                x2, y1+radius,
+                x2, y2-radius,
+                x2, y2-radius,
+                x2, y2,
+                x2-radius, y2,
+                x2-radius, y2,
+                x1+radius, y2,
+                x1+radius, y2,
+                x1, y2,
+                x1, y2-radius,
+                x1, y2-radius,
+                x1, y1+radius,
+                x1, y1+radius,
+                x1, y1]
+
+        if not update:
+            return self.create_polygon(points, **kwargs, smooth=True)
+        
+        else:
+            self.coords(self.rect, points)
+
+    def resize(self, event):
+        text_bbox = self.bbox(self.text)
+
+        if self.radius > event.width or self.radius > event.height:
+            radius = min((event.width, event.height))
+
+        else:
+            radius = self.radius
+
+        width, height = event.width, event.height
+
+        if event.width < text_bbox[2]-text_bbox[0]:
+            width = text_bbox[2]-text_bbox[0] + 30
+        
+        if event.height < text_bbox[3]-text_bbox[1]:  
+            height = text_bbox[3]-text_bbox[1] + 30
+        
+        self.round_rectangle(5, 5, width-5, height-5, radius, update=True)
+
+        bbox = self.bbox(self.rect)
+
+        x = ((bbox[2]-bbox[0])/2) - ((text_bbox[2]-text_bbox[0])/2)
+        y = ((bbox[3]-bbox[1])/2) - ((text_bbox[3]-text_bbox[1])/2)
+
+        self.moveto(self.text, x, y)
+
+    def border(self, event):
+        if event.type == "4":
+            self.itemconfig(self.rect, fill="#d2d6d3")
+            if self.clicked is not None:
+                self.clicked()
+
+        else:
+            self.itemconfig(self.rect, fill=self.btnbackground)
+
+def func():
+    print("Bouton cliqué")
+
+# root = tk.Tk()
+# btn = RoundedButton(text="", radius=100, btnbackground="#0078ff", btnforeground="#ffffff", clicked=func)
+# btn.pack(expand=True, fill="both")
+# root.mainloop()
