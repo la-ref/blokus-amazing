5b93c7e89e5c6566c0293ba4270f111f11ad9182
MisterGranti67 <mathsgovlog@gmail;com>
2022-11-23 16:01:32 +0100

    Accueil fait sans hover

--- accueil.py
+++ accueil.py
@@ -4,7 +4,7 @@ import sys
 from Elements.bouton.button import *
 
 class Accueil(Frame):
-    def __init__(self,image_list):
+    def __init__(self,window,image_list):
         super()
         # OUTPUT_PATH = Path(__file__).parent
         # ASSETS_PATH = OUTPUT_PATH / Path(r"Accueil/assets/frame0")
@@ -12,7 +12,7 @@ class Accueil(Frame):
 
         # def relative_to_assets(path: str) -> Path:
         #     return ASSETS_PATH / Path(path)
-
+        self.window = window
         canvas = Canvas(
             window,
             bg = "#FFFFFF",
@@ -24,82 +24,76 @@ class Accueil(Frame):
         )
 
         canvas.place(x = 0, y = 0)
-        image_1 = canvas.create_image(
+        background = canvas.create_image(
             720.0,
             512.0,
             image=image_list[0]
         )
-        def quitGame(event):
-            window.destroy()
 
         # button_load = RoundedButton(window, text="Hors ligne", radius=100, hauteur=128, largueur=500, fontsize=32, btnbackground="#0078ff", btnforeground="#ffffff", clicked=func)
 
 
-        quitButton = canvas.create_image(
+        HorsLigneBouton = canvas.create_image(
             470, 
             324, 
             image=image_list[5],
             anchor=tk.NW
         )
-        canvas.tag_bind(quitButton, "<Button-1>", quitGame)
+        # canvas.tag_bind(HorsLigneBouton, "<Button-1>", self.HorsLigneBouton)
 
 
-        button_2 = Button(
+        EnLigneBouton = canvas.create_image(
+            470, 
+            488, 
             image=image_list[1],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_2 clicked"),
-            relief="flat"
+            anchor=tk.NW
         )
+        # canvas.tag_bind(EnLigneBouton, "<Button-1>", self.EnLigneBouton)
 
-        button_2.place(
-            x=470.0,
-            y=488.0,
-            width=500.0,
-            height=128.0
-        )
 
-        button_3 = Button(
+
+        QuitterBouton = canvas.create_image(
+            470, 
+            652, 
             image=image_list[2],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_3 clicked"),
-            relief="flat"
+            anchor=tk.NW
         )
-        button_3.place(
-            x=470.0,
-            y=652.0,
-            width=500.0,
-            height=128.0
-        )
+        canvas.tag_bind(QuitterBouton, "<Button-1>", self.QuitterBouton)
 
-        button_4 = Button(
+        
+
+
+        BoutonScore = canvas.create_image(
+            1032, 
+            821, 
             image=image_list[3],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_4 clicked"),
-            relief="flat"
+            anchor=tk.NW
         )
-        button_4.place(
-            x=1032.0,
-            y=821.0,
-            width=80.0,
-            height=80.0
-        )
+        # canvas.tag_bind(BoutonScore, "<Button-1>", self.BoutonScore)
 
-        button_5 = Button(
+
+        BoutonInfo = canvas.create_image(
+            334, 
+            821, 
             image=image_list[4],
-            borderwidth=0,
-            highlightthickness=0,
-            command=lambda: print("button_5 clicked"),
-            relief="flat"
+            anchor=tk.NW
         )
-        button_5.place(
-            x=334.0,
-            y=821.0,
-            width=80.0,
-            height=80.0
-        )
+        # canvas.tag_bind(BoutonInfo, "<Button-1>", self.BoutonInfo)
+
+    def QuitterBouton(self,event):
+        self.window.destroy()
+    
+    def BoutonInfo(self,event):
+        self.window.destroy()
+    
+    def BoutonScore(self,event):
+        self.window.destroy()
+    
+    def EnLigneBouton(self,event):
+        self.window.destroy()
+    
+    def HorsLigneBouton(self,event):
+        self.window.destroy()
 
 
 if __name__ == "__main__":
@@ -118,7 +112,9 @@ if __name__ == "__main__":
     image.append(PhotoImage(file="Accueil/assets/frame0/button_5.png"))
     image.append(PhotoImage(file="Accueil/assets/frame0/button_1.png"))
 
-    MonAccueil = Accueil(image)
+
+
+    MonAccueil = Accueil(window,image)
     # MonAccueil.pack()
 
     window.mainloop()
--- build/gui.py
+++ build/gui.py
@@ -39,164 +39,88 @@ image_image_1 = PhotoImage(
     file=relative_to_assets("image_1.png"))
 image_1 = canvas.create_image(
     720.0,
-    512.0,
+    541.0,
     image=image_image_1
 )
 
-image_image_2 = PhotoImage(
-    file=relative_to_assets("image_2.png"))
-image_2 = canvas.create_image(
-    232.0,
-    733.0,
-    image=image_image_2
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat"
+)
+button_1.place(
+    x=479.0,
+    y=324.0,
+    width=482.0,
+    height=128.0
 )
 
-image_image_3 = PhotoImage(
-    file=relative_to_assets("image_3.png"))
-image_3 = canvas.create_image(
-    1201.0,
-    740.0,
-    image=image_image_3
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat"
+)
+button_2.place(
+    x=479.0,
+    y=488.0,
+    width=491.0,
+    height=128.0
 )
 
-image_image_4 = PhotoImage(
-    file=relative_to_assets("image_4.png"))
-image_4 = canvas.create_image(
-    1201.0,
-    298.0,
-    image=image_image_4
+button_image_3 = PhotoImage(
+    file=relative_to_assets("button_3.png"))
+button_3 = Button(
+    image=button_image_3,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_3 clicked"),
+    relief="flat"
+)
+button_3.place(
+    x=479.0,
+    y=652.0,
+    width=482.0,
+    height=128.0
 )
 
-image_image_5 = PhotoImage(
-    file=relative_to_assets("image_5.png"))
-image_5 = canvas.create_image(
-    1200.0,
-    894.0,
-    image=image_image_5
+button_image_4 = PhotoImage(
+    file=relative_to_assets("button_4.png"))
+button_4 = Button(
+    image=button_image_4,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_4 clicked"),
+    relief="flat"
+)
+button_4.place(
+    x=1032.0,
+    y=821.0,
+    width=80.0,
+    height=80.0
 )
 
-canvas.create_text(
-    1110.783935546875,
-    853.0,
-    anchor="nw",
-    text="MIREILLE",
-    fill="#FFFFFF",
-    font=("LilitaOne", 32 * -1)
+button_image_5 = PhotoImage(
+    file=relative_to_assets("button_5.png"))
+button_5 = Button(
+    image=button_image_5,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_5 clicked"),
+    relief="flat"
 )
-
-image_image_6 = PhotoImage(
-    file=relative_to_assets("image_6.png"))
-image_6 = canvas.create_image(
-    231.0,
-    888.0,
-    image=image_image_6
-)
-
-canvas.create_text(
-    141.78399658203125,
-    847.0,
-    anchor="nw",
-    text="JACK",
-    fill="#FFFFFF",
-    font=("LilitaOne", 32 * -1)
-)
-
-image_image_7 = PhotoImage(
-    file=relative_to_assets("image_7.png"))
-image_7 = canvas.create_image(
-    716.0,
-    894.0,
-    image=image_image_7
-)
-
-image_image_8 = PhotoImage(
-    file=relative_to_assets("image_8.png"))
-image_8 = canvas.create_image(
-    720.0,
-    135.0,
-    image=image_image_8
-)
-
-image_image_9 = PhotoImage(
-    file=relative_to_assets("image_9.png"))
-image_9 = canvas.create_image(
-    232.0,
-    289.0,
-    image=image_image_9
-)
-
-image_image_10 = PhotoImage(
-    file=relative_to_assets("image_10.png"))
-image_10 = canvas.create_image(
-    230.0,
-    135.0,
-    image=image_image_10
-)
-
-canvas.create_text(
-    140.78399658203125,
-    94.0,
-    anchor="nw",
-    text="FRANÇOIS",
-    fill="#FFFFFF",
-    font=("LilitaOne", 32 * -1)
-)
-
-image_image_11 = PhotoImage(
-    file=relative_to_assets("image_11.png"))
-image_11 = canvas.create_image(
-    1204.0,
-    146.0,
-    image=image_image_11
-)
-
-canvas.create_text(
-    1114.783935546875,
-    105.0,
-    anchor="nw",
-    text="HUGETTE",
-    fill="#FFFFFF",
-    font=("LilitaOne", 32 * -1)
-)
-
-image_image_12 = PhotoImage(
-    file=relative_to_assets("image_12.png"))
-image_12 = canvas.create_image(
-    716.0,
-    509.0,
-    image=image_image_12
-)
-
-image_image_13 = PhotoImage(
-    file=relative_to_assets("image_13.png"))
-image_13 = canvas.create_image(
-    226.0,
-    331.0,
-    image=image_image_13
-)
-
-image_image_14 = PhotoImage(
-    file=relative_to_assets("image_14.png"))
-image_14 = canvas.create_image(
-    227.0,
-    688.0,
-    image=image_image_14
-)
-
-image_image_15 = PhotoImage(
-    file=relative_to_assets("image_15.png"))
-image_15 = canvas.create_image(
-    1200.0,
-    344.0,
-    image=image_image_15
-)
-
-image_image_16 = PhotoImage(
-    file=relative_to_assets("image_16.png"))
-image_16 = canvas.create_image(
-    1194.0,
-    692.0,
-    image=image_image_16
+button_5.place(
+    x=334.0,
+    y=821.0,
+    width=80.0,
+    height=80.0
 )
 window.resizable(False, False)
 window.mainloop()
