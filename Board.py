31201494c9bfa0548d55e2805c23ae572a3c792e
la-ref <jmermoud81@gmail.com>
2022-11-25 15:57:03 +0100

    Ajout du fonctionnement et vérification des placements de chaque pièces conformément aux règles du jeu

--- Board.py
+++ Board.py
@@ -1,12 +1,13 @@
-import numpy as np
 from __future__ import annotations
+import numpy as np
 from Player import Player 
+from Pieces import Pieces
 class Board:
 
     def __init__(self : Board, size : int) -> None:
-        self.__size : int = size | 20
+        self.__size : int = size
         self.__board = np.empty( (self.__size,self.__size), dtype=Player)
-        self.__board.fill(np.nan)
+        self.__board.fill(0)
 
     def __checkBoardLimit(self : Board, row : int, column : int) -> bool:
         return ((row < self.__size and row >= 0) and (column < self.__size and column >= 0))
@@ -17,7 +18,7 @@ class Board:
     
     def isEmptyAt(self : Board, row : int, column : int) -> bool:
         if (self.__checkBoardLimit(row, column)):
-            return self.__board[row][column] == np.nan
+            return self.__board[row][column] == 0
         return False
     
     def addPlayerAt(self : Board, player : Player, row : int, column : int) -> None:
@@ -31,4 +32,36 @@ class Board:
                     or (row == self.__size-1 and column == 0)
                     or (row == 0 and column == self.__size-1))
         return False
-            
+
+    def __verifyApplicationStart(self: Board,x,y,player : Player) -> bool:
+        return player.getNbTour() == 0 and self.isInCorner(y,x)
+
+    def verifyApplication(self: Board,piece : Pieces,column:int,row:int,player : Player,declage : int) -> bool:
+        x : int = column-declage
+        y : int = row-declage
+        delimitation : np.ndarray = piece.getDelimitation()
+        nbCorners : int = piece.getNbCorners()
+        countCorners : int = 0
+        cornerReduction : int = 0
+        for i in range(len(delimitation)):
+            for v in range(len(delimitation)):
+                if ((y+i >=0) and (y+i < self.__size) and (x+v >=0) and (x+v < self.__size)):
+                    if (delimitation[i][v] == 3 and self.__board[y+i][x+v] > 0):
+                        return False
+                    if (delimitation[i][v] == 1 and self.__board[y+i][x+v] > 0 and self.__board[y+i][x+v] == player.getColor()): 
+                        return False
+                    if (delimitation[i][v] == 2):
+                        if (self.__board[y+i][x+v] != player.getColor()):
+                            countCorners+= 1
+                else:
+                    if (delimitation[i][v] == 3):
+                        return False
+                    if (delimitation[i][v] == 2):
+                        cornerReduction+=1
+
+        if delimitation[declage][declage] == 3 and self.__verifyApplicationStart(x+declage,y+declage,player):
+            return True
+        return (not ((nbCorners-cornerReduction) == countCorners))
+    
+    def getBoard(self) -> np.ndarray:
+        return self.__board
---
+++ Pieces.py
@@ -0,0 +1,58 @@
+import numpy as np
+from __future__ import annotations
+class Pieces:
+    def __init__(self: Pieces, forme : np.ndarray, identifiant: int) -> None:
+        self.__id : int = identifiant
+        self.__forme : np.ndarray = forme
+        self.__delimitation : np.ndarray = self.__findCorners()
+    
+    def __findCorners(self : Pieces):
+        delim : np.ndarray = np.empty( (len(self.__forme)+2,len(self.__forme[0])+2), dtype=int)
+        delim.fill(1)
+        for i in range(len(self.__forme)):
+            for y in range(len(self.__forme[0])):
+                if self.__forme[i][y]:
+                    delim[i+1][y+1] = 3
+        for i in range(len(delim)):
+            for y in range(len(delim[0])):
+                countCorners : int = 0
+                for v in [-1,1]:
+                    for k in [-1,1]:
+                        if ((i+v >= 0 and i+v <= len(delim)-1) and (y+k >= 0 and y+k <= len(delim[0])-1)):
+                            if delim[i+v][y+k] == 3:
+                                countCorners+=1
+                borderCounter : int = 0
+                noneCounter : int = 0
+                for k in [-1,1]:
+                    if (i+k >= 0 and i+k <= len(delim)-1):
+                            if delim[i+k][y] == 3 and  countCorners > 0:
+                                borderCounter+=1
+                            elif countCorners == 0 and delim[i+k][y] == 3:
+                                noneCounter+=1
+                    if (y+k >= 0 and y+k <= len(delim[0])-1):
+                            if delim[i][y+k] == 3 and  countCorners > 0:
+                                borderCounter+=1
+                            elif countCorners == 0 and delim[i][y+k] == 3:
+                                noneCounter+=1
+                if borderCounter == 0 and countCorners > 0:
+                    delim[i][y] = 2
+                elif noneCounter == 0 and countCorners == 0:
+                    delim[i][y] = 0      
+        return delim
+
+    def getDelimitation(self: Pieces) -> np.ndarray:
+        return self.__delimitation
+
+    def rotate90(self : Pieces) -> None:
+        self.__delimitation : np.ndarray = np.rot90(self.__delimitation,1,axes=(1,0))
+        self.__forme : np.ndarray = np.rot90(self.__delimitation,1,axes=(1,0))
+
+    def flip(self : Pieces) -> None:
+        self.__delimitation : np.ndarray = np.fliplr(self.__delimitation)
+        self.__forme : np.ndarray = np.fliplr(self.__delimitation)
+
+    def getNbCorners(self : Pieces) -> int:
+        return np.count_nonzero(self.__delimitation == 2)
+
+    def getForme(self : Pieces) -> np.ndarray:
+        return self.__forme
