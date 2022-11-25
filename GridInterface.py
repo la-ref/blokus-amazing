9e6564ae306df49206926e1994437f7285815bb0
Haffner Aurélian <aurelian.haffner@sfr.fr>
2022-11-25 11:19:15 +0100

    PlayerListGui partie cosmetique finie

--- GridInterface.py
+++ GridInterface.py
@@ -21,28 +21,28 @@ if __name__=="__main__":
     
     List1 = tk.Frame(window)
     
-    window.columnconfigure(0, weight=1)
-    window.columnconfigure(1, weight=1)
-    window.columnconfigure(2, weight=1)
+    # window.columnconfigure(0, weight=1)
+    # window.columnconfigure(1, weight=1)
+    # window.columnconfigure(2, weight=1)
     
-    List1.config(width=250,height=450 , bg="yellow")
-    List1.grid(row = 0, column = 0, pady = 2, padx=2)
+    # List1.config(width=250,height=450 , bg="yellow")
+    # List1.grid(row = 0, column = 0, pady = 2, padx=2)
     
-    List2 = tk.Frame(window)
-    List2.config(width=250,height=450 , bg="green")
-    List2.grid(row = 0, column = 2, pady = 2, padx=2)
+    # List2 = tk.Frame(window)
+    # List2.config(width=250,height=450 , bg="green")
+    # List2.grid(row = 0, column = 2, pady = 2, padx=2)
     
-    List3 = tk.Frame(window)
-    List3.config(width=250,height=450 , bg="blue")
-    List3.grid(row = 1, column = 2, pady = 2, padx=2)
+    # List3 = tk.Frame(window)
+    # List3.config(width=250,height=450 , bg="blue")
+    # List3.grid(row = 1, column = 2, pady = 2, padx=2)
     
-    List4 = tk.Frame(window)
-    List4.config(width=250,height=450 , bg="red")
-    List4.grid(row = 1, column = 0, pady = 2, padx=2)
+    # List4 = tk.Frame(window)
+    # List4.config(width=250,height=450 , bg="red")
+    # List4.grid(row = 1, column = 0, pady = 2, padx=2)
     
 
 
     board = GridInterface(window,7)
-    board.grid(row = 0, rowspan=2 , column = 1, pady = 2, padx=2)
+    board.place("1440x1024")
 
     window.mainloop()
