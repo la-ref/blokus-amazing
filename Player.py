1989ed5dbc3ef5c42535f54adcbf7b3481f370bd
la-ref <jmermoud81@gmail.com>
2022-12-01 15:38:32 +0100

    ajout pièces classe joueur

--- Player.py
+++ Player.py
@@ -1,9 +1,12 @@
+from PiecesDeclaration import LISTEPIECES
+from Pieces import Pieces
 class Player:
     nbJoueur = 1
     def __init__(self, idCouleur : int|None,nom: str|None):
         self.__name : str =  nom or ("Joueur"+str(Player.nbJoueur))
         self.__idCouleur : int = idCouleur or 1
         self.__nbTour : int = 0 
+        self.__pieces : dict[str,Pieces] = LISTEPIECES.copy()
         Player.nbJoueur +=1
 
     @property
