ff0776d6dd2e8d7aec009d3266ffd56e1c11ed5f
la-ref <jmermoud81@gmail.com>
2022-12-07 17:42:58 +0100

    ajout affichage de l'affichage des pieces sur le plateau et fix bug pour poser les pièces dans les coins

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from Player import Player
 from Board import Board
+from GridInterface import GridInterface
 class Game:
     """Classe de gestion des parties de jeu blokus
     """
@@ -13,7 +14,7 @@ class Game:
             plateau (Board | None): plateau de jeu ou rien
             taille (int): taille du plateau de jeu
         """
-        self.__joueurs : list[Player] = joueurs or [Player(11,"matthieu"),Player(12,"aurelian"),Player(13,"gauthier"),Player(14,"inconnu")]
+        self.__joueurs : list[Player] = joueurs or [Player(11,"matthieu"),Player(12,"aurelian"),Player(13,"gauthier")]#,Player(14,"inconnu")]
         self.__plateau : Board = plateau or Board(taille)
     
     def getPlayers(self : Game):
@@ -39,7 +40,52 @@ class Game:
         return self.__plateau
 
     def jeu(self : Game):
-        while True:
+        from tkinter import PhotoImage
+        import tkinter as tk
+        import Elements.PiecesListGUI as PG
+        import MouvementManager as Mv
+        from PIL import ImageTk
+    
+        window = tk.Tk()
+        images = []
+
+        images.append(ImageTk.PhotoImage(file="build/assets/frame0/empty_list.png"))
+        images.append(PhotoImage(file="build/assets/frame0/player_yellow.png"))
+        images.append(PhotoImage(file="build/assets/frame0/player_green.png"))
+        images.append(PhotoImage(file="build/assets/frame0/player_blue.png"))
+        images.append(PhotoImage(file="build/assets/frame0/player_red.png"))
+        images.append(PhotoImage(file="build/assets/frame0/AppBorder.png"))
+        images.append(PhotoImage(file="build/assets/frame0/board.png"))#270x270
+        
+        window.geometry("1440x1024")
+        
+        border = tk.Canvas()
+        border.config(bg="white")
+        border.create_image(
+                0,
+                0,
+                image=images[5],
+                anchor=tk.NW
+            )
+        border.place(x=0,y=0,height=1024,width=1440,anchor=tk.NW)
+        board = GridInterface(border,self.__plateau,images)
+        board.move(x=720-270,y=512-270)
+        
+        List1 = PG.PiecesListGUI(border,images,"Joueur 1",1)
+        List1.move(x=70,y=80)
+        
+        List2 = PG.PiecesListGUI(border,images,"Joueur 2",2)
+        List2.move(x=1047,y=80)
+        
+        List3 = PG.PiecesListGUI(border,images,"Joueur 3",3)
+        List3.move(x=1047,y=524)
+        
+        List4 = PG.PiecesListGUI(border,images,"Joueur 4",4)
+        List4.move(x=70,y=524)
+        Mv.MouvementManager(List4,True,True)
+        
+
+        def task():
             for joueur in self.__joueurs:
                 print("c'est a : ",joueur.getName())
                 ajout = False
@@ -55,4 +101,8 @@ class Game:
                     joueur.ajoutTour()
                     print(str(pieceid))
                     joueur.removePiece(str(pieceid))
-                print(self.__plateau.getBoard())
+                board.refreshBoard()
+            window.update()
+
+        while 1:
+            task()  
